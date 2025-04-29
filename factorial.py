911813ec0a4a6ada5d5976cd2c007fa85e79acc5
srimathi-2006 <srimathisaravanan23@gmail.com>
2025-04-29 11:41:18 +0530

    Update recursion

--- factorial.py
+++ factorial.py
@@ -1,7 +1,7 @@
-def factorial(n): 
-  result=1
-  for i in range(2,n+1):
-    result*=i
-  return result
+def factorial(n):
+  if n==0 or n==1:
+    return 1
+  else:
+    return n*factorial(n-1)
 num=int(input("Enter a nuber:"))
 print(f"Factorial of{num}is{factorial(num)}") 
